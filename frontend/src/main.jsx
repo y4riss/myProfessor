f6ac9360f9b8ec840e5c4fd8ad7032b580574724
joessir <lassiryyass@gmail.com>
2023-12-06 18:44:21 +0100

    changed router to new router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,31 +1,45 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { UserProvider } from "./context/userProvider.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Professors from "./views/Professors";
 import Home from "./views/Home";
 import Login from "./views/Login";
 import About from "./views/About";
 import SingleProfessor from "./views/SingleProfessor";
 import MyEvaluations from "./views/MyEvaluations";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/professors",
+    element: <Professors />,
+  },
+  {
+    path: "/myevaluations",
+    element: <MyEvaluations />,
+  },
+  {
+    path: "/professors/:id",
+    element: <SingleProfessor />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <UserProvider>
     <GoogleOAuthProvider clientId="271756417186-5heann651h9anu7ahtv2ckfo2onetene.apps.googleusercontent.com">
-      <React.StrictMode>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/professors" element={<Professors />} />
-            <Route path="/myevaluations" element={<MyEvaluations />} />
-            <Route path="/professors/:id" element={<SingleProfessor />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </BrowserRouter>
-      </React.StrictMode>
+      <RouterProvider router={router} />
     </GoogleOAuthProvider>
   </UserProvider>
 );
